3978b8d6f521b742f88f3cc07ece7d9980ad7e0d
yusdesign <swiftcase@gmail.com>
2025-11-05 13:53:53 +0800

    remove shell scripts

--- build.sh
+++
@@ -1,6 +0,0 @@
-# Build the project
-cargo build --target aarch64-linux-android --release
-
-# Verify build
-find target -name "*.so" -type f
-file target/aarch64-linux-android/release/libaudiovisualizer.so
